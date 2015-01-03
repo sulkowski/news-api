ac940bef91231a796b9f1637cb86d3c7b41d5d7d
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2015-01-04 00:57:17 +0100

    [#5] - Improve the way of loading files in `lib/app.rb`

--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,11 @@
+$LOAD_PATH << File.expand_path('../../', __FILE__)
+$LOAD_PATH << File.expand_path('../news/v1', __FILE__)
+
 require 'sinatra/base'
 require 'sinatra/json'
 
-require_relative 'news/v1/routes/stories'
-require_relative 'news/v1/routes/users'
+require 'routes/stories'
+require 'routes/users'
 
 class App < Sinatra::Base
   use News::V1::Routes::Stories
