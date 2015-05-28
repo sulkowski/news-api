5500bf86241c75ced81d0e67834bf2ba86f394c7
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2015-05-28 22:11:23 +0200

    [#2] - Remove unnecessary if for `user.save!`

--- app/routes/users.rb
+++ app/routes/users.rb
@@ -4,10 +4,9 @@ module News
       namespace '/users' do
         post do
           user = User.new(email: params['email'], password: params['password'])
-          if user.save!
-            status 201
-            json user.serializable_hash(only: [:id, :email])
-          end
+          user.save!
+          status 201
+          json user.serializable_hash(only: [:id, :email])
         end
       end
     end
