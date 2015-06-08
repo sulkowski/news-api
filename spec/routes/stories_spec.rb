fb90b45e77df858ce014d1dbb3fd3ef72a656dec
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2015-06-08 16:29:49 +0200

    Improve readibility of stories spec

--- spec/routes/stories_spec.rb
+++ spec/routes/stories_spec.rb
@@ -1,30 +1,30 @@
 require 'spec_helper'
 
 describe News::Routes::Stories do
-  let(:user) { User.create(email: 'm@mi6.co.uk', password: 'james') }
-  let!(:story) do
-    Story.create(
+  let(:user_params) {
+    { email: 'm@mi6.co.uk', password: 'james' }
+  }
+  let(:story_params) {
+    {
       id: 1,
       user: user,
       title: 'Lorem ipsum',
       url: 'http://www.lipsum.com/'
-    )
-  end
+    }
+  }
 
   describe '#GET `/stories`' do
+    let(:user)   { User.create(user_params) }
+    let!(:story) { Story.create(story_params) }
+
     before { get '/stories' }
 
-    it 'returns 200 status code' do
-      expect(last_response.status).to eq(200)
-    end
+    it_should_behave_like 'json response'
 
-    it 'has `application/json` content type' do
-      expect(last_response.header['Content-Type']).to include('application/json')
-    end
-
-    it 'returns stories' do
+    it 'returns correct status code and stories' do
       first_story = JSON.parse(last_response.body).first
 
+      expect(last_response.status).to eq(200)
       expect(first_story).to include_json(
         id: 1,
         title: 'Lorem ipsum',
@@ -43,24 +43,15 @@ describe News::Routes::Stories do
     end
 
     context 'when params are valid' do
-      before do
-        sign_in
-        post '/stories', title: 'React.js', url: 'https://facebook.github.io/react/'
-      end
+      before { sign_in }
+      before { post '/stories', title: 'React.js', url: 'https://facebook.github.io/react/' }
 
       it_should_behave_like 'authorized user'
       it_should_behave_like 'json response'
 
-      it 'returns `201` status code' do
+      it 'returns correct status code, location header and a new story in response' do
         expect(last_response.status).to eq(201)
-      end
-
-      it 'contains `location` header' do
-        expect(last_response.header).to have_key('Location')
         expect(last_response.header['Location']).to match(%r{\/stories\/\d+})
-      end
-
-      it 'returns a new story in response' do
         expect(last_response.body).to include_json(
           title: 'React.js',
           url: 'https://facebook.github.io/react/',
@@ -70,72 +61,39 @@ describe News::Routes::Stories do
       end
     end
 
-    describe 'url validations' do
-      context 'when is not given' do
-        before do
-          sign_in
-          post '/stories', title: 'React.js'
-        end
+    context 'when params are not valid' do
+      before { sign_in }
+      before { post '/stories' }
 
-        it_should_behave_like 'json response'
+      it_should_behave_like 'json response'
 
-        it 'returns 422 status code' do
-          expect(last_response.status).to eq(422)
-        end
-
-        it 'returns error description' do
-          expect(last_response.body).to include_json(
-            error: {
-              code: 422,
-              message: 'Validation failed: Url can\'t be blank',
-              errors: {
-                url: ['can\'t be blank']
-              }
+      it 'returns correct status code and error description' do
+        expect(last_response.status).to eq(422)
+        expect(last_response.body).to include_json(
+          error: {
+            code: 422,
+            message: 'Validation failed: Title can\'t be blank, Url can\'t be blank',
+            errors: {
+              title: ['can\'t be blank'],
+              url: ['can\'t be blank']
             }
-          )
-        end
-      end
-    end
-
-    describe 'title validations' do
-      context 'when is not given' do
-        before do
-          sign_in
-          post '/stories', url: 'https://facebook.github.io/react/'
-        end
-
-        it_should_behave_like 'json response'
-
-        it 'returns 422 status code' do
-          expect(last_response.status).to eq(422)
-        end
-
-        it 'returns error description' do
-          expect(last_response.body).to include_json(
-            error: {
-              code: 422,
-              message: 'Validation failed: Title can\'t be blank',
-              errors: {
-                title: ['can\'t be blank']
-              }
-            }
-          )
-        end
+          }
+        )
       end
     end
   end
 
   describe '#GET `/stories/:id`' do
     context 'when the story exists' do
+      let(:user)   { User.create(user_params) }
+      let!(:story) { Story.create(story_params) }
+
       before { get '/stories/1' }
 
       it_should_behave_like 'json response'
 
-      it 'returns a successful response' do
-        expect(last_response).to be_ok
-      end
-
-      it 'returns story as a json' do
+      it 'returns correct status code story' do
+        expect(last_response.status).to eq(200)
         expect(last_response.body).to include_json(
           id: 1,
           title: 'Lorem ipsum',
@@ -147,19 +105,16 @@ describe News::Routes::Stories do
     end
 
     context 'when the story does not exist' do
-      before { get '/stories/0' }
-
       it_should_behave_like 'json response'
 
-      it 'returns `not found` response' do
-        expect(last_response.status).to eq(404)
-      end
+      before { get '/stories/1' }
 
-      it 'contains error message' do
+      it 'returns correct status code and error message' do
+        expect(last_response.status).to eq(404)
         expect(last_response.body).to include_json(
           error: {
             code: 404,
-            message: 'Couldn\'t find News::Models::Story with \'id\'=0'
+            message: 'Couldn\'t find News::Models::Story with \'id\'=1'
           }
         )
       end
@@ -168,16 +123,16 @@ describe News::Routes::Stories do
 
   describe '#PATCH `/stories/:id`' do
     context 'when didn`t crate the story' do
+      let(:user)   { User.create(user_params) }
+      let!(:story) { Story.create(story_params) }
+
       before { sign_in }
       before { patch '/stories/1' }
 
       it_should_behave_like 'json response'
 
-      it 'returns `403` status code' do
+      it 'returns correct status code and error message' do
         expect(last_response.status).to eq(403)
-      end
-
-      it 'contains error message' do
         expect(last_response.body).to include_json(
           error: {
             code: 403,
@@ -187,200 +142,131 @@ describe News::Routes::Stories do
       end
     end
 
-    context 'when the user created the story' do
-      let(:user) { sign_in }
+    context 'when the story belongs to the user' do
+      let(:user)   { sign_in }
+      let!(:story) { Story.create(story_params) }
 
-      describe 'updating `title`' do
-        before { patch '/stories/1', title: 'React.js' }
+      before { patch '/stories/1', title: 'React.js', url: 'https://facebook.github.io/react/' }
 
-        it_should_behave_like 'authorized user'
-        it_should_behave_like 'json response'
+      it_should_behave_like 'authorized user'
+      it_should_behave_like 'json response'
 
-        it 'has `200` status code' do
-          expect(last_response.status).to eq(200)
-        end
-
-        it 'updates the attribute' do
-          expect(last_response.body).to include_json(
-            id: 1,
-            title: 'React.js',
-            url: 'http://www.lipsum.com/',
-            likes: 0,
-            dislikes: 0
-          )
-        end
-      end
-
-      describe 'updating `url`' do
-        before { patch '/stories/1', url: 'https://facebook.github.io/react/' }
-
-        it_should_behave_like 'authorized user'
-        it_should_behave_like 'json response'
-
-        it 'has `200` status code' do
-          expect(last_response.status).to eq(200)
-        end
-
-        it 'updates the attribute' do
-          expect(last_response.body).to include_json(
-            id: 1,
-            title: 'Lorem ipsum',
-            url: 'https://facebook.github.io/react/',
-            likes: 0,
-            dislikes: 0
-          )
-        end
+      it 'returns correct status code adn updates the attributes' do
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to include_json(
+          id: 1,
+          title: 'React.js',
+          url: 'https://facebook.github.io/react/',
+          likes: 0,
+          dislikes: 0
+        )
       end
     end
   end
 
   describe '#GET `/stories/:id/url`' do
     context 'when a stroy does not exist' do
-      before do
-        get '/stories/0/url'
-      end
+      before { get '/stories/1/url' }
 
-      it 'returns `not found` response' do
+      it 'returns 404 status code' do
         expect(last_response.status).to eq(404)
       end
     end
 
     context 'when a story exists' do
-      before do
-        get '/stories/1/url'
-      end
+      let(:user)   { User.create(user_params) }
+      let!(:story) { Story.create(story_params) }
 
-      it 'sets `location` header to the story url' do
+      before { get '/stories/1/url' }
+
+      it 'returns correct status code and location header' do
         expect(last_response.location).to eq('http://www.lipsum.com/')
-      end
-
-      it 'sets response status to `303' do
         expect(last_response.status).to eq(303)
       end
     end
   end
 
   describe '#PUT `/stories/:id/vote/up`' do
-    let!(:current_user) { sign_in }
+    let(:user)   { sign_in }
+    let!(:story) { Story.create(story_params) }
+
+    before { put '/stories/1/vote/up' }
+
+    it_should_behave_like 'authorized user'
+
+    context 'when a user has not voted for a story' do
+      it 'returns correct status code changes count of votes for a stroy' do
+        expect(last_response.status).to eq(204)
+        expect(story.likes).to eq(1)
+      end
+    end
 
     context 'when a user already voted for a story' do
-      let!(:vote) { Vote.create(user: @current_user, story: story, delta: 1) }
+      let!(:vote) { Vote.create(user: user, story: story, delta: 1) }
 
-      before { put '/stories/1/vote/up' }
-
-      it_should_behave_like 'authorized user'
-
-      it 'has `204` status code' do
+      it 'returns correct status code and does not change count of votes' do
         expect(last_response.status).to eq(204)
-      end
-
-      it 'does not change count of votes for a story' do
         expect(story.likes).to eq(1)
       end
     end
 
     context 'when user already voted against the story' do
-      let!(:vote) { Vote.create(user: @current_user, story: story, delta: -1) }
+      let!(:vote) { Vote.create(user: user, story: story, delta: -1) }
 
-      before { put '/stories/1/vote/up' }
-
-      it_should_behave_like 'authorized user'
-
-      it 'has `204` status code' do
+      it 'returns correct status code and changes count of likes and dislikes' do
         expect(last_response.status).to eq(204)
-      end
-
-      it 'changes count of likes for the story' do
         expect(story.likes).to eq(1)
-      end
-
-      it 'changes count of dislikes for the story' do
         expect(story.dislikes).to eq(0)
-      end
-    end
-
-    context 'when a user has not voted for a story' do
-      before { put '/stories/1/vote/up' }
-
-      it_should_behave_like 'authorized user'
-
-      it 'has `204` status code' do
-        expect(last_response.status).to eq(204)
-      end
-
-      it 'changes count of votes for a stroy' do
-        expect(story.likes).to eq(1)
       end
     end
   end
 
   describe '#PUT `/stories/:id/vote/down`' do
-    let!(:current_user) { sign_in }
+    let(:user)   { sign_in }
+    let!(:story) { Story.create(story_params) }
+
+    before { put '/stories/1/vote/down' }
+
+    it_should_behave_like 'authorized user'
 
     context 'when a user already voted against a story' do
-      let!(:vote) { Vote.create(user: @current_user, story: story, delta: -1) }
+      let!(:vote) { Vote.create(user: user, story: story, delta: -1) }
 
-      before { put '/stories/1/vote/down' }
-
-      it_should_behave_like 'authorized user'
-
-      it 'has `204` status code' do
+      it 'returns correct status code does not change count of votes' do
         expect(last_response.status).to eq(204)
-      end
-
-      it 'does not change count of votes for a story' do
         expect(story.dislikes).to eq(1)
       end
     end
 
     context 'when user already voted for the story' do
-      let!(:vote) { Vote.create(user: @current_user, story: story, delta: 1) }
+      let!(:vote) { Vote.create(user: user, story: story, delta: 1) }
 
-      before { put '/stories/1/vote/down' }
-
-      it_should_behave_like 'authorized user'
-
-      it 'has `204` status code' do
+      it 'returns correct status code and changes count of likes and dislikes' do
         expect(last_response.status).to eq(204)
-      end
-
-      it 'changes count of dislikes for the story' do
         expect(story.dislikes).to eq(1)
-      end
-
-      it 'changes count of likes for the story' do
         expect(story.likes).to eq(0)
       end
     end
 
     context 'when a user has not voted against a story' do
-      before { put '/stories/1/vote/down' }
-
-      it_should_behave_like 'authorized user'
-
-      it 'has `204` status code' do
+      it 'returns correct status code and changes count of dislikes' do
         expect(last_response.status).to eq(204)
-      end
-
-      it 'changes count of votes for a stroy' do
         expect(story.dislikes).to eq(1)
       end
     end
   end
 
   describe '#DELETE `stories/:id/vote`' do
-    let!(:current_user) { sign_in }
-    let!(:vote) { Vote.create(user: @current_user, story: story, delta: 1) }
+    let(:user)   { sign_in }
+    let!(:story) { Story.create(story_params) }
+    let!(:vote)  { Vote.create(user: user, story: story, delta: 1) }
 
     before { delete 'stories/1/vote' }
 
     it_should_behave_like 'authorized user'
 
-    it 'has `204` status code' do
+    it 'returns correct status code and removes vote of a user' do
       expect(last_response.status).to eq(204)
-    end
-
-    it 'removes vote of a user' do
       expect(story.likes).to eq(0)
     end
   end
